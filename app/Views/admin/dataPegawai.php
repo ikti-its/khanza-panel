d2b23abcf5385dd995dded8a1910f41424c4ab36
rubenboediono <rubentricahya@gmail.com>
2024-07-05 00:06:53 +0700

    commit pegawai clear

--- app/Views/admin/dataPegawai.php
+++ app/Views/admin/dataPegawai.php
@@ -21,7 +21,7 @@
 
                         <div>
                             <div class="inline-flex gap-x-2">
-                              
+
                                 <a href='/tambahpegawai' class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-teal-600 text-white hover:bg-teal-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
                                     <svg class="flex-shrink-0 size-3" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                         <path d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
@@ -89,6 +89,8 @@
                                 foreach ($alamat_data as $alamat) {
                                     if ($alamat['id_akun'] === $pegawaiEntry['id_akun']) {
                                         $alamatEntry = $alamat;
+                                        
+                                        
                                         break;
                                     }
                                 }
@@ -333,6 +335,7 @@
                                                                 <!-- End Col -->
                                                             </div>
 
+
                                                             <div class="sm:flex sm:items-center py-4">
                                                                 <!-- Grid -->
                                                                 <div class="sm:w-1/6">
@@ -364,7 +367,7 @@
                                                                 </div>
                                                                 <!-- End Col -->
                                                             </div>
-                                                         
+
 
                                                         </div>
                                                     </div>
