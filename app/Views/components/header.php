d37fc99a7fee8b0c3825ef520078e12e61179be6
rubenboediono <rubentricahya@gmail.com>
2024-07-05 23:01:23 +0700

    COMMIT HEADER

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -67,18 +67,7 @@
                                 <p class="text-sm text-gray-500 dark:text-gray-400">Masuk sebagai</p>
                                 <p class="text-sm font-medium text-gray-800 dark:text-gray-300"><?php echo session('user_details')['email'] ?></p>
                             </div>
-                            <div class="mt-2 py-2 first:pt-0 last:pb-0">
-
-                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="/profile">
-                                    <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                                        <circle cx="9" cy="7" r="4" />
-                                        <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                                        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                    </svg>
-                                    Lihat profil
-                                </a>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
